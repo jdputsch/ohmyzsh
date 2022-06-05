701b34d25bb9537953c01d34fd64d596ab05dd87
Jeff Putsch <jeff.putsch@analog.com>
2022-06-04 19:23:12 -0700

    add jdputsch theme to customize prompt, fix python environmet file

--- custom/python.zsh
+++ custom/python.zsh
@@ -7,9 +7,6 @@
 
 export PIP_REQUIRE_VIRTUALENV=true
 
-# If /usr/sepp/bin does not exist, then enable pyenv
-[ ! -d /usr/sepp/bin ] && disable-pyenv
-
 # Local Variables:
 # mode: sh
 # eval: (sh-set-shell "zsh")
---
+++ custom/themes/jdputsch.zsh-theme
@@ -0,0 +1,4 @@
+# Put your custom themes in this folder.
+# Example:
+
+PROMPT="%{$fg[blue]%}%n%{$reset_color%}@%U%{$fg[yellow]%}%m%{$reset_color%}%u %% "
